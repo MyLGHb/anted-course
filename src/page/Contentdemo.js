30151e348254eba98604a9cf0e02947d39dc6820
myihao <myihao@linewell.com>
2020-07-22 09:23:08 +0800

    添加自定义组件及组件属性使用，引入antDesign组件

---
+++ src/page/Contentdemo.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+import ShoppingList from './ShoppingList.js';
+
+class Content extends React.Component {
+  render() {
+    return (
+      <ShoppingList name="张三" />
+    );
+  }
+}
+
+export default Content;
