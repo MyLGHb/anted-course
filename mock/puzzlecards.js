f77068df321c2254da25442798eeec7b50049e3d
myihao <myihao@linewell.com>
2020-07-29 10:16:55 +0800

    模拟请求出错

--- mock/puzzlecards.js
+++ mock/puzzlecards.js
@@ -15,12 +15,29 @@ const random_jokes = [
 
 let random_joke_call_count = 0;
 
+//模拟请求后端返回数据
+// export default {
+//   'get /dev/posts/1': function (req, res) {
+//     const responseObj = random_jokes[random_joke_call_count % random_jokes.length];
+//     random_joke_call_count += 1;
+//     setTimeout(() => {
+//       res.json(responseObj);
+//     }, 3000);
+//   },
+// };
+
+//模拟请求出错
 export default {
   'get /dev/posts/1': function (req, res) {
-    const responseObj = random_jokes[random_joke_call_count % random_jokes.length];
-    random_joke_call_count += 1;
-    setTimeout(() => {
-      res.json(responseObj);
-    }, 3000);
+    res.status(500);
+    res.json({});
   },
 };
+
+//如果只需要简单地返回数据，只需要这样
+// export default {
+//   'get /dev/random_joke': {
+//     setup: 'What is the object oriented way to get wealthy ?',
+//     punchline: 'Inheritance',
+//   },
+// };
--- src/model/puzzlecards.js
+++ src/model/puzzlecards.js
@@ -4,6 +4,7 @@
  */
 
 import request from '../util/request';
+import { message } from 'antd';
 
 const delay = (millisecond) => {
   return new Promise((resolve) => {
@@ -22,14 +23,18 @@ export default {
       const {call, put} = sagaEffects;
       const endPointURI = '/dev/posts/1';
       
-      
-      const puzzle = yield call(request, endPointURI);
-      yield put({ type: 'addNewCard', payload: puzzle });
-      
-      yield call(delay, 3000);
+      try {
+        const puzzle = yield call(request, endPointURI);
+        yield put({ type: 'addNewCard', payload: puzzle });
+        
+        yield call(delay, 3000);
 
-      const puzzle2 = yield call(request, endPointURI);
-      yield put({ type: 'addNewCard', payload: puzzle2 });
+        const puzzle2 = yield call(request, endPointURI);
+        yield put({ type: 'addNewCard', payload: puzzle2 });
+      } catch (e) {
+        message.error('数据获取失败'); // 打印错误信息
+      };
+      
     }
   },
   reducers: {
